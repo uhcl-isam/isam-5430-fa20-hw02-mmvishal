a0ccb0cda6c5ea10c8d2880e6aae377776b300e2
mallam5664 <35759418+mmvishal@users.noreply.github.com>
2020-09-26 11:04:53 -0500

    SquareUp Commit

--- CSharp.Assignments.Loop1/SquareUp.cs
+++ CSharp.Assignments.Loop1/SquareUp.cs
@@ -14,11 +14,34 @@ namespace CSharp.Assignments.Loop1
         /// for n=3, the app will print out 0, 0, 1,    0, 2, 1,    3, 2, 1 on separate lines
         /// (spaces added to show the 3 groups).
         /// </summary>
-        public static void Main()
-        {
-            int x = int.Parse(Console.ReadLine());
-            Console.WriteLine(x);
+         public static void Main()
+            {
+                Console.Write("Enter a number to display a square up: ");
+                int number = int.Parse(Console.ReadLine());
+                // Write your codes here.
+                int n1 = number - 1;
+                if (number >= 0)
+                {
+                    int n = 1;
+                    do
+                    {
+                        for (int k = 0; k < n1; k++)
+                        {
+                            Console.Write("0");
+                            Console.Write(" ");
+                        }
 
-        }
+                        for (int i = n; i > 0; i--)
+                        {
+                            Console.Write(i);
+                            Console.Write(" ");
+                        }
+                        Console.WriteLine();
+                        n++;
+                        n1--;
+                    } while (n <= number);
+                }
+
+            }
     }
 }
